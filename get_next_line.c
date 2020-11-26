b348e3cca488adc5f045ab57430dca10353c2203
Dylan POINSU <dpoinsu@e3r11p15.42.fr>
2020-11-26 16:41:44 +0100

    str + i to save after \n

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: dpoinsu </var/mail/dpoinsu>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/26 15:40:33 by dpoinsu           #+#    #+#             */
-/*   Updated: 2020/11/26 16:11:08 by dpoinsu          ###   ########.fr       */
+/*   Updated: 2020/11/26 16:40:12 by dpoinsu          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,17 +16,25 @@
 int	get_next_line(int fd, char **line)
 {
 	static void *str;
+	char		*tmp;
 
 	i = 0;
-	while (fd, ((str = read(fd, str, BUFFER_SIZE)) == 1))
+	if (str[i] != '\0')
+	{
+		i = ft_strlen(str);
+		*line = (char*)str;
+		str = str + i;
+	}
+	while (fd && read(fd, str, BUFFER_SIZE) == 1)
 	{
 		i = 0;
 		while (str[i])
 		{
 			if (str[i] == '\n')
 			{
-				strncpy(*line, str, i);
-				
+				strncpy(tmp, str, i);
+				strcat(line, tmp);
+				str = str + i;
 			}
 			i++;
 		}
