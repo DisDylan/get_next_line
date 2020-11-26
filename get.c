f8d28c76a0dee1927d12a30b63e7d55800f8b55c
Dylan POINSU <dpoinsu@e2r11p9.42.fr>
2020-11-26 19:43:50 +0100

    added get.c to test with only one function

--- get.c
+++ get.c
@@ -1,24 +1,39 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*   get.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dpoinsu </var/mail/dpoinsu>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/11/26 15:46:17 by dpoinsu           #+#    #+#             */
-/*   Updated: 2020/11/26 19:31:07 by dpoinsu          ###   ########.fr       */
+/*   Created: 2020/11/26 19:40:34 by dpoinsu           #+#    #+#             */
+/*   Updated: 2020/11/26 19:41:25 by dpoinsu          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
+#include "get_next_line.h"
 
-# define GET_NEXT_LINE_H
+int get_next_line(int fd, char **line)
+{
+	static char *save;
+	char *tmp;
+	int i;
 
-# include <stdlib.h>
-# include <unistd.h>
-
-# define BUFF_SIZE 32
-
-int		get_next_line(int fd, char **line);
-
-#endif
+	i = 0;
+	if (!fd || !*line)
+		return (-1);
+	if (!save)
+	{
+		while (read(fd, save, BUFF_SIZE))
+			i++;
+	}
+	while (save[i] != '\n')
+	{
+		tmp[i] = save[i];
+		i++;
+	}
+	save = save + i;
+	*line = tmp;
+	if (save[i] == '\0')
+		return (0);
+	return (1);
+}
