f7f37c29e3d0b799185097869846cb65a7175341
Dylan POINSU <dpoinsu@e3r12p9.42.fr>
2020-11-27 14:55:57 +0100

    some modif on get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: dpoinsu <dpoinsu@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/26 19:50:15 by dpoinsu           #+#    #+#             */
-/*   Updated: 2020/11/27 14:21:01 by dpoinsu          ###   ########.fr       */
+/*   Updated: 2020/11/27 14:55:37 by dpoinsu          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,14 +29,17 @@ int get_next_line(int fd, char **line)
 			i++;
 	}
 	i = 0;
-	while (save[i] != '\n')
+	if (save[i] == '\0')
+	{
+		strcpy(*line, save);
+		return (0);
+	}
+	while (save[i] != '\n' || save[i] != '\0')
 	{
 		tmp[i] = save[i];
 		i++;
 	}
 	strcpy(save, save + i);
-	*line = tmp;
-	if (save[i] == '\0')
-		return (0);
+	strcpy(*line, tmp);
 	return (1);
 }
