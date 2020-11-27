836791048915a3a0c620247a6a8921c5f9c083f7
Dylan POINSU <dpoinsu@e3r12p9.42.fr>
2020-11-27 14:21:24 +0100

    no segfault but bus error

--- get_next_line.c
+++ get_next_line.c
@@ -6,22 +6,24 @@
 /*   By: dpoinsu <dpoinsu@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/26 19:50:15 by dpoinsu           #+#    #+#             */
-/*   Updated: 2020/11/27 14:10:08 by dpoinsu          ###   ########.fr       */
+/*   Updated: 2020/11/27 14:21:01 by dpoinsu          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+#include <string.h>
 
 int get_next_line(int fd, char **line)
 {
-	static char *save;
+	static char save[1000];
 	char *tmp;
 	int i;
 
 	i = 0;
+	tmp = "";
 	if (!fd || !line)
 		return (-1);
-	if (!save)
+	if (save[0] == '\0')
 	{
 		while (read(fd, save, BUFF_SIZE) >= 1)
 			i++;
@@ -32,7 +34,7 @@ int get_next_line(int fd, char **line)
 		tmp[i] = save[i];
 		i++;
 	}
-	save = save + i;
+	strcpy(save, save + i);
 	*line = tmp;
 	if (save[i] == '\0')
 		return (0);
