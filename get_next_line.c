60d25c02c10f6581c520a6c751148eb9678325f3
Dylan POINSU <dpoinsu@e2r11p5.42.fr>
2020-12-01 09:59:48 +0100

    utils ok

--- get_next_line.c
+++ get_next_line.c
@@ -6,40 +6,62 @@
 /*   By: dpoinsu <dpoinsu@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/26 19:50:15 by dpoinsu           #+#    #+#             */
-/*   Updated: 2020/11/27 14:55:37 by dpoinsu          ###   ########.fr       */
+/*   Updated: 2020/12/01 09:59:35 by dpoinsu          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#include <string.h>
 
-int get_next_line(int fd, char **line)
+static char	*ft_strdup(char *str)
 {
-	static char save[1000];
-	char *tmp;
-	int i;
 
-	i = 0;
-	tmp = "";
-	if (!fd || !line)
+}
+
+static int	process_line(char **backup, char **line, char *nl_str)
+{
+	char*tmp;
+
+	if (nl_str != NULL)
+	{
+		*line = ft_strndup(*backup, nl_str - *backup);
+		tmp = ft_strndup(nl_str + 1, ft_strlen(nl_str + 1));
+		free(*backup);
+		*backup = tmp;
+		return (1);
+	}
+	if (*backup != NULL)
+	{
+		*line = *backup;
+		*backup = NULL;
+	}
+	else
+		*line = ft_strndup("", 1);
+	return (0);
+}
+
+int		get_next_line(int fd, char **line)
+{
+	static char*backup[256];
+	static charbuffer[BUFFER_SIZE + 1];
+	char*tmp;
+	intlen_read;
+	char*nl_str;
+
+	if (fd < 0 || line == NULL || BUFFER_SIZE <= 0)
 		return (-1);
-	if (save[0] == '\0')
+	while ((nl_str = ft_strchr(backup[fd], '\n')) == 0
+			&& (len_read = read(fd, buffer, BUFFER_SIZE)) > 0)
 	{
-		while (read(fd, save, BUFF_SIZE) >= 1)
-			i++;
+		buffer[len_read] = 0;
+		if (backup[fd] == NULL)
+			tmp = ft_strndup(buffer, len_read);
+		else
+			tmp = ft_strjoin(backup[fd], buffer);
+		if (backup[fd] != 0)
+			free(backup[fd]);
+		backup[fd] = tmp;
 	}
-	i = 0;
-	if (save[i] == '\0')
-	{
-		strcpy(*line, save);
-		return (0);
-	}
-	while (save[i] != '\n' || save[i] != '\0')
-	{
-		tmp[i] = save[i];
-		i++;
-	}
-	strcpy(save, save + i);
-	strcpy(*line, tmp);
-	return (1);
+	if (len_read < 0)
+		return (-1);
+	return (process_line(&backup[fd], line, nl_str));
 }
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: dpoinsu <dpoinsu@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/27 14:12:33 by dpoinsu           #+#    #+#             */
-/*   Updated: 2020/11/30 15:30:26 by dpoinsu          ###   ########.fr       */
+/*   Updated: 2020/12/01 09:55:23 by dpoinsu          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,23 +40,37 @@ char	*ft_strchr(const char *str, int c)
 	return (NULL);
 }
 
-char	*ft_strcpy(char *dest, char *src)
+void	*ft_strcpy(char *dest, char *src)
 {
 	size_t i;
 
 	i = -1;
-	if (!src)
-		return (dest);
-	while (++i < n)
+	while (src[++i])
 		dest[i] = src[i];
-	return (dest);
+}
+
+void	*ft_strcat(char *dest, char *src)
+{
+	size_t i;
+	size_t j;
+
+	i = -1;
+	j = ft_strlen(dest);
+	while (src[++i])
+		dest[++j] = src[i];
 }
 
 char	*ft_strjoin(char *s1, char *s2)
 {
+	char *str;
 
+	if (!s1 && !s2)
+		return (NULL);
+	if (!(str = (char*)malloc(sizeof(str) *
+					(ft_strlen(s1) + ft_strlen(s2) + 1))))
+		return (NULL);
+	ft_strcpy(str, s1);
+	ft_strcat(str, s2);
+	str[ft_strlen(str) + 1] = '\0';
+	return (str);
 }
-
-strdup
-
-strcat
